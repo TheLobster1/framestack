624c6d3eb3ba561b9f2cd64ef446c8dcbc2252ec
TheLobster1 <rjeveldman@hotmail.nl>
2025-03-22 17:22:59 +0100

    Change layout and add commands to registerpage

--- framestack/Platforms/Android/MainApplication.cs
+++ framestack/Platforms/Android/MainApplication.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.Runtime;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace framestack
 {
@@ -9,6 +11,17 @@ namespace framestack
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+            {
+                if (view is Entry)
+                {
+                    // Remove underline
+                    handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+
+                    // Change placeholder text color
+                    // handler.PlatformView.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.Red));
+                }
+            });
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
--- framestack/ViewModels/RegisterPageViewModel.cs
+++ framestack/ViewModels/RegisterPageViewModel.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
 
 namespace framestack.ViewModels
 {
     public partial class RegisterPageViewModel : ViewModel
     {
+        private bool allowedUsername = false;
+        private bool allowedFirstname = false;
+        private bool allowedLastname = false;
+        private bool allowedEmail = false;
+        private bool allowedPassword = false;
+        private bool allowedDateOfBirth = false;
+
+        private CancellationTokenSource UsernameDatabaseCheckToken = new CancellationTokenSource();
+        private CancellationTokenSource EmailDatabaseCheckToken = new CancellationTokenSource();
+        
+        [RelayCommand]
+        private async Task UpdateUsernameValue()
+        {
+            await Application.Current.MainPage.Navigation.PopAsync();
+
+        }
+        
+        // private async Task Update
     }
 }
