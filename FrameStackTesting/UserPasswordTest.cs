bf7705b0aab1ac09b6662c0b992acd49631e8037
Andreikova <andrei.khoudiakov@student.nhlstenden.com>
2025-03-26 16:10:33 +0100

    User class implementation

---
+++ FrameStackTesting/UserPasswordTest.cs
@@ -0,0 +1,13 @@
+﻿namespace FrameStackTesting
+{
+    using 
+    public class UserPasswordTest
+    {
+        [Fact]
+        public void verifyPasswordTest()
+        {
+            User someOne = new User("Billy", )
+            Assert.True()
+        }
+    }
+}
