fa2f08f5eeabb2714af91bebeaa1e7e1cf177688
TheLobster1 <rjeveldman@hotmail.nl>
2025-03-31 19:29:45 +0200

    Adjustments to API

--- framestackAPI/Program.cs
+++ framestackAPI/Program.cs
@@ -1,3 +1,8 @@
+using framestackAPI;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -35,6 +40,21 @@ app.MapGet("/picturesfromaccount/{accountId}/{page}/{size}", (HttpRequest reques
     return pictures;
 })
     .WithName("GetPictures");
+
+// app.MapPost("/createuser",)
+
+app.MapPost("/uploadpicture/{accountId}", async Task<Results<Ok<string>, BadRequest<string>>>
+    ([FromForm] FormFile fileUploadForm, HttpContext context, IAntiforgery antiforgery, HttpRequest request) =>
+{
+    var idObject = request.RouteValues["accountId"];
+    if (idObject == null) return TypedResults.BadRequest("Please provide a valid account Id");
+    var id = idObject.ToString();
+    if (fileUploadForm == null) return TypedResults.BadRequest("Please provide a file");
+    if (fileUploadForm.FileName == null || fileUploadForm.FileName == "") return TypedResults.BadRequest("Please provide a file name");
+    await Utils.UploadFile(fileUploadForm, id);
+
+    return TypedResults.Ok($"Uploaded file {fileUploadForm.FileName} successfully");
+});
 
 app.Run();
 
---
+++ framestackAPI/Utils.cs
@@ -0,0 +1,21 @@
+﻿namespace framestackAPI;
+
+public static class Utils
+{
+    //TODO: EXPLAIN WHAT THIS DOES
+    public static async Task UploadFile(FormFile fileUploadForm, string accountId)
+    {
+        string pathToSave = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+        if (!Directory.Exists(pathToSave)) Directory.CreateDirectory(pathToSave);
+        var userPath = Path.Combine(pathToSave, accountId);
+        if (!Directory.Exists(userPath)) Directory.CreateDirectory(userPath);
+        string fullFilePath = Path.Combine(userPath, Path.GetRandomFileName());
+        while (File.Exists(fullFilePath))
+        {
+            fullFilePath = Path.Combine(userPath, Path.GetRandomFileName());
+        }
+
+        await using FileStream stream = new (fullFilePath, FileMode.Create);
+        await fileUploadForm.CopyToAsync(stream);
+    }
+}
