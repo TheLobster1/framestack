4ba4a18d7fa239a44ee66d796a1b2d0c554bd1db
TheLobster1 <rjeveldman@hotmail.nl>
2025-04-13 21:24:39 +0200

    Implemented threading for running multiple API calls at the same time.

--- framestack/ViewModels/HomePageViewModel.cs
+++ framestack/ViewModels/HomePageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Core.Extensions;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using framestack.Models;
 using framestack.Services;
@@ -9,28 +11,33 @@ namespace framestack.ViewModels;
 public partial class HomePageViewModel : ViewModel
 {
     [ObservableProperty]
-    public List<Picture> pictures;
+    public ObservableCollection<Picture> pictures;
     [ObservableProperty]
-    private readonly LocalUserStorage localUserStorage;
+    private LocalUserStorage localUserStorage;
 
     public HomePageViewModel()
     {
         localUserStorage = Application.Current.Windows[0].Page.Handler.MauiContext.Services.GetService<LocalUserStorage>();
         try
         {
-            Pictures = localUserStorage.User.getPictureList();
+            if (LocalUserStorage.Pictures == null)
+            {
+                LocalUserStorage.Pictures = new List<Picture>();
+            }
+            Pictures = LocalUserStorage.User.getPictureList().ToObservableCollection();
         }
         catch (Exception ex)
         {
             //Something happened.
         }
-        GetPictures();
+        GetAllPictures();
     }
 
     private async Task GetPictures()
     {
-        Pictures = await RestService.GetPictures(localUserStorage.User.eMail);
-        localUserStorage.Pictures = Pictures;
+        var pictureList = await RestService.GetPictures(LocalUserStorage.User.eMail);
+        Pictures = pictureList.ToObservableCollection();
+        LocalUserStorage.Pictures = pictureList;
     }
 
     [RelayCommand]
@@ -51,6 +58,50 @@ public partial class HomePageViewModel : ViewModel
     private async Task UploadPicture(FileResult picture)
     {
         await RestService.UploadPicture(picture, localUserStorage.User, []);
-        GetPictures();
+        GetAllPictures();
     }
+
+    private async Task GetAllPictures(int startPage = 0)
+    {
+        if (startPage == 0)
+        {
+            LocalUserStorage.Pictures.Clear();
+            Pictures.Clear();
+        }
+        bool hasMoreContent = true;
+        var tasks = new List<Task<List<Picture>>>();
+        int amountOfPages = startPage + 10;
+        for (startPage = startPage; startPage < amountOfPages; startPage++)
+        {
+            tasks.Add(RestService.GetPictures(localUserStorage.User.eMail, startPage));
+        }
+        await Task.WhenAll(tasks);
+        foreach (var task in tasks)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                foreach (var picture in task.Result)
+                {
+                    Pictures.Add(picture);
+                }
+                LocalUserStorage.Pictures = task.Result;
+            });
+            
+            if (task.Result.Count < 20)
+            {
+                hasMoreContent = false;
+            }
+        }
+
+        if (hasMoreContent)
+        {
+            GetAllPictures(startPage);
+        }
+    }
+
+    // static async Task<List<Picture>> ThreadPictureLoad(string email, int page)
+    // {
+    //     var pictures = await RestService.GetPictures(email, page);
+    //     return pictures;
+    // }
 }
